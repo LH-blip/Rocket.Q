f938c56ed01429ad97877d851250d5937c3fd279
Luis Henrique <luishenrique18109@gmail.com>
2021-07-04 18:29:32 -0300

    mod routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 
-const route = express.Router()
+const route = express.Router();
 
 route.get('/', (require, response) => response.render('index'))
+route.get('/room', (req, res) => res.render('room'));
+route.get('/create-pass', (req, res) => res.render('create-pass'));
 
 module.exports = route;
